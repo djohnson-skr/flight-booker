d4b5660907ef4dbc83b8319f337b7a5b1c2f08e4
Armin Efendic <arminefendic12@gmail.com>
2022-03-02 20:36:33 -0500

    Fixed sytnax issue

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -4,6 +4,6 @@ class Airport < ApplicationRecord
                                dependent: :destroy
 
   has_many :arriving_flights, class_name: "Flight", 
-                              foreign_key: "arrival_airport_id"
+                              foreign_key: "arrival_airport_id",
                               dependent: :destroy
 end
